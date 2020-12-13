3d65a3bae3709aa46006195e1cb3f3073938b786
Luis Bezerra <52874192+joseluisbezerra@users.noreply.github.com>
2020-12-13 19:05:48 -0300

    Pong game

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pong
+O jogo pong feito com o módulo Turtle do python
+
+<p align="center">
+  <img src="/img/pong.gif" />
+</p>
---
+++ pong.py
@@ -0,0 +1,137 @@
+# Simple pong game made with the Turtle module
+
+import turtle
+import time
+
+# FPS adjustment
+_tick2_frame = 0
+_tick2_fps = 20000000 # real raw FPS
+_tick2_t0 = time.time()
+
+def tick(fps=60):
+    global _tick2_frame, _tick2_fps, _tick2_t0
+    n = _tick2_fps / fps
+    _tick2_frame += n
+    while n > 0: 
+        n -= 1
+        if time.time() - _tick2_t0 > 1:
+            _tick2_t0 = time.time()
+            _tick2_fps = _tick2_frame
+            _tick2_frame = 0
+
+
+# Motion functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Screen settings
+wn = turtle.Screen()
+wn.title('Pong')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup() # Turtle will move around the screen, but will not draw when its pen state is PENUP. The turtle's default pen state is PENDOWN.
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('square')
+ball.color('white')
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
+while True:
+    tick()
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border cheking
+    # Top and bottom
+    if (ball.ycor() > 290):
+        ball.sety(290)
+        ball.dy *= -1
+    elif (ball.ycor() < -290):
+        ball.sety(-290)
+        ball.dy *= -1
+    # Left and right
+    if (ball.xcor() > 390):
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+    elif (ball.xcor() < -390):
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 60 and ball.ycor() > paddle_b.ycor() - 60):
+        ball.setx(340)
+        ball.dx *= -1
+    elif (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 60 and ball.ycor() > paddle_a.ycor() -60):
+        ball.setx(-340)
+        ball.dx *= -1
